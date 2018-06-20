e247ef9c354528002044407f075260ba2b80df6b
Punanova <punanovaanna@gmail.com>
2018-06-20 20:46:01 +0500

    first commit

---
+++ o-_and_p-_NH2D/test_pNH2D.py
@@ -0,0 +1,22 @@
+import pyspeckit
+from pyspeckit.spectrum.models import nh2d
+import numpy as np
+
+import astropy.units as u
+
+cube = pyspeckit.Spectrum('h-mm1_pnh2d.fits')
+rms=0.07
+cube.error[:] = rms
+cube.xarr.refX = 110153.594e6*u.Hz
+cube.xarr.velocity_convention = 'radio'
+cube.xarr.convert_to_unit('km/s')
+F=False
+T=True
+import matplotlib.pyplot as plt
+plt.ion()
+cube.Registry.add_fitter('nh2d_vtau', pyspeckit.models.nh2d.nh2d_vtau_fitter,4)
+cube.specfit(fittype='nh2d_vtau', guesses=[3.94, 0.1, 0, 0.1], 
+    verbose_level=4, signal_cut=1.5, limitedmax=[F,T,T,T], limitedmin=[T,T,T,T], 
+    minpars=[0, 0, -1, 0.05], maxpars=[30.,50.,1,0.5], fixed=[F,F,F,F])
+cube.plotter(errstyle='fill')
+cube.specfit.plot_fit()
